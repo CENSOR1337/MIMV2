23585eaf0eb67f0f06086de905d91659709339b2
MeteeLaptop <viixxivvimm@gmail.com>
2020-10-01 16:04:06 +0700

    no message

--- Administrator/csv.php
+++ Administrator/csv.php
@@ -1,33 +1,50 @@
 <?php
-$CsvHandle = fopen("test.csv", "r");
-$TargetString = array("Identify", "ชื่อ", "นามสกุล");
-$SortingIndex = [];
 
-$Return = [];
-$Row = 1;
-if (($CsvHandle) !== false) {
-    $CsvData = fgetcsv($CsvHandle, 1000, ",");
+$AccpetedExt = array("csv");
+if ($_POST) {
+    if (isset($_FILES['upload'])) {
 
-    for ($i = 0; $i < count($TargetString); $i++) {
-        if (array_search($TargetString[$i], $CsvData)) {
-            array_push($SortingIndex, array_search($TargetString[$i], $CsvData));
+        $File = $_FILES['upload']['name'];
+        $TempFile = $_FILES['upload']['tmp_name'];
+        $ext = explode(".", $File);
+        $ext = $ext[count($ext) - 1];
+    }
+    if (in_array($ext, $AccpetedExt)) {
+
+        $CsvHandle = fopen($TempFile, "r");
+        $TargetString = array("เลขประจำตัวประชาชน", "Identify", "เลขประจำตัวนักเรียน", "ชื่อ", "นามสกุล");
+        $SortingIndex = [];
+        $Return = [];
+
+        if (($CsvHandle) !== false) {
+            $CsvData = fgetcsv($CsvHandle, 1000, ",");
+
+            for ($i = 0; $i < count($TargetString); $i++) {
+                if (array_search($TargetString[$i], $CsvData)) {
+                    array_push($SortingIndex, array_search($TargetString[$i], $CsvData));
+                }
+            }
+
+            while (($CsvData = fgetcsv($CsvHandle, 1000, ",")) !== false) {
+                $ResultArr = [];
+                foreach ($SortingIndex as &$Value) {
+                    if (!(empty($CsvData[$Value]))) {
+                        array_push($ResultArr, $CsvData[$Value]);
+                    }
+                }
+                if (!(empty($CsvData[$Value]))) {
+                    array_push($Return, $ResultArr);
+                }
+            }
+
+            foreach ($Return as &$Value) {
+                for ($i = 0; $i < count($SortingIndex); $i++) {
+                    echo $Value[$i] . " ";
+                }
+                echo "<br>";
+            }
+
+            fclose($CsvHandle);
         }
     }
-
-    while (($CsvData = fgetcsv($CsvHandle, 1000, ",")) !== false) {
-
-        $ResultArr = [];
-        foreach ($SortingIndex as &$Value) {
-            array_push($ResultArr,$CsvData[$Value]);
-        }
-        array_push($Return, $ResultArr);
-    }
-
-    foreach ($Return as &$Value) {
-        echo $Value[0] . "<br>";
-        echo $Value[1] . "<br>";
-        echo $Value[2] . "<br>";
-    }
-
-    fclose($CsvHandle);
 }
