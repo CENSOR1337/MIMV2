053b6671cee85811749861619dc9e67a7c81891e
MeteeLaptop <viixxivvimm@gmail.com>
2020-10-08 14:41:35 +0700

    no message

--- PHPFunction/doRegistion.php
+++ PHPFunction/doRegistion.php
@@ -1,11 +1,21 @@
 <?php
 ob_start();
 ob_end_clean();
+
 if (isset($_POST['Identify'], $_POST['Firstname'], $_POST['Lastname'], $_POST['Email'],
     $_POST['Gender'], $_POST['BirthDays'], $_POST['BirthMonths'], $_POST['BirthYears'],
     $_POST['AcademicYear'], $_POST['Gender'], $_POST['Room'], $_POST['Degree'])) {
     require_once '../config/connects.php';
     $Identify = mysqli_real_escape_string($conn, $_POST['Identify']);
+
+    $sql = "SELECT * FROM `Profiles` WHERE `IndentifyID` LIKE '$Identify'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $ReturnValue = array("Status" => 'registered');
+        print json_encode($ReturnValue);
+        return;
+    }
+
     $StudentID = mysqli_real_escape_string($conn, $_POST['StudentID']);
     $Firstname = mysqli_real_escape_string($conn, $_POST['Firstname']);
     $Lastname = mysqli_real_escape_string($conn, $_POST['Lastname']);
@@ -25,21 +35,26 @@ if (isset($_POST['Identify'], $_POST['Firstname'], $_POST['Lastname'], $_POST['E
         $sql = "INSERT INTO `Profiles` (`IndentifyID`, `StudentID`, `Firstname`, `Lastname`,
         `Gender`, `BirthDate`, `Degree`, `Room`, `AcademicYear`, `permission`)
         VALUES ('$Identify', '$StudentID', '$Firstname', '$Lastname', '$Gender', '$BirthDate', '$Degree', '$Room',
-        '$AcademicYear', 'pending')";
+        '$AcademicYear', 'pending');";
     } else {
         $sql = "INSERT INTO `Profiles` (`IndentifyID`, `Firstname`, `Lastname`,
         `Gender`, `BirthDate`, `Degree`, `Room`, `AcademicYear`, `permission`)
         VALUES ('$Identify', '$Firstname', '$Lastname', '$Gender', '$BirthDate', '$Degree', '$Room',
-        '$AcademicYear', 'pending')";
+        '$AcademicYear', 'pending');";
     }
 
-    if ($conn->query($sql) === true) {
-        print json_encode('Done!');
+    $sql .= "UPDATE `AccessibleProfiles` SET `Firstname` = '$Firstname', `Lastname` = '$Lastname' WHERE `AccessibleProfiles`.`IndentifyID` = '$Identify'";
+
+    if ($conn->multi_query($sql)) {
+        $ReturnValue = array("Status" => 'success');
     } else {
-        print json_encode($sql . "<br>" . $conn->error);
+        $ReturnValue = array("Status" => 'error');
     }
+
+    print json_encode($ReturnValue);
 
     $conn->close();
+
 } else {
     print json_encode(false);
 }
