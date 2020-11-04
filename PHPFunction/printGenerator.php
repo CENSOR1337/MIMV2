31bb200ce1c523062af0021e86eb4225ac36124b
MeteeTanathip <viixxivvimm@gmail.com>
2020-11-05 00:54:54 +0700

    no message

--- PHPFunction/printGenerator.php
+++ PHPFunction/printGenerator.php
@@ -7,16 +7,33 @@
     <title>เอกสารยืนยันขอใช้งานระบบ</title>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="../css/font.css">
 
 </head>
+
 <style>
     td,
     th {
-        border: 1px solid black;
+        border: 1px solid gray;
     }
+    @page { margin: 0; }
+    @media print {
+  @page { margin: 0; }
+  body { margin: 1.6cm; }
+}
 </style>
 
-<body>
+<script>
+    function startprint(){
+
+        if(true){
+        window.print();
+        setTimeout(window.close, 0);
+    }
+    };
+</script>
+
+<body onload="startprint()">
     <div class="container">
 
         <table>
@@ -31,47 +48,47 @@
                     </td>
                     <td class="center-align" colspan='3'>
                             <h6>แบบฟอร์มการขอใช้งานบริการอินเทอร์เน็ต<br>โรงเรียนเมืองยมวิทยาคาร</h6>
-                    
+
                     </td>
                 </tr>
 
                 <tr>
                     <td colspan="1" class="right-align"><b>เลขประจำตัวประชาชน : </b></td>
-                    <td colspan="3"> IdentifyID </td>
+                    <td colspan="3"> <?php echo $_GET['identifyID']; ?> </td>
                 </tr>
                 <tr>
                     <td colspan="1" class="right-align"><b>เลขประจำตัวนักเรียน : </b></td>
-                    <td colspan="3"> answers[0] </td>
+                    <td colspan="3"> <?php echo $_GET['studentID']; ?> </td>
                 </tr>
                 <tr>
                     <td colspan="1" class="right-align"><b>ชื่อ - สกุล : </b></td>
-                    <td colspan="3"> answers[1] answers[2] </td>
+                    <td colspan="3"> <?php echo $_GET['fullName']; ?> </td>
                 </tr>
                 <tr>
                     <td colspan="1" class="right-align"><b>วันเกิด : </b></td>
-                    <td colspan="3"> BirthDateNew </td>
+                    <td colspan="3"> <?php echo $_GET['birthDate']; ?> </td>
                 </tr>
                 <tr>
                     <td colspan="1" class="right-align"><b>อีเมล : </b></td>
-                    <td colspan="3"> answers[6] </td>
+                    <td colspan="3"> <?php echo $_GET['email']; ?> </td>
                 </tr>
                 <tr>
-                    <td class="right-align"><b>เพศ : </b></td>
-                    <td> Gender </td>
-                    <td class="right-left"><b>ปีการศึกษา : </b></td>
-                    <td> answers[8] </td>
+                    <td width="30%" class="right-align"><b>เพศ : </b></td>
+                    <td width="23%"> <?php echo $_GET['gender']; ?> </td>
+                    <td width="23%" class="right-align"><b>ปีการศึกษา : </b></td>
+                    <td width="22%"> <?php echo $_GET['academicYear']; ?> </td>
                 </tr>
                 <tr>
                     <td class="right-align"><b>ประเภทผู้ใช้ : </b></td>
-                    <td> Role </td>
-                    <td class="right-left"><b>สถานะ : </b></td>
-                    <td> Permission </td>
+                    <td > <?php echo $_GET['status']; ?> </td>
+                    <td class="right-align"><b>สถานะ : </b></td>
+                    <td> <?php echo $_GET['permission']; ?> </td>
                 </tr>
                 <tr>
                     <td class="right-align"><b>มัธยมศึกษาปีที่ : </b></td>
-                    <td> answers[11] </td>
-                    <td class="right-left"><b>ห้องที่ : </b></td>
-                    <td> answers[12] </td>
+                    <td> <?php echo $_GET['studentYear']; ?> </td>
+                    <td class="right-align"><b>ห้องที่ : </b></td>
+                    <td> <?php echo $_GET['room']; ?> </td>
                 </tr>
                 <tr>
                     <td colspan="4">
@@ -80,10 +97,11 @@
                     </td>
                 </tr>
                 <tr>
-                    <td colspan="2" class="center-align" height="150px" width="50%">
+
+                    <td colspan="2" class="center-align" height="150px" width="50%"><br><br>
                         ลงชื่อ............................................................<br><br>
                         (............................................................)<br>(ผู้ขอใช้บริการ)<br>วันที่................/................/................</td>
-                    <td colspan="2" class="center-align" width="50%">
+                    <td colspan="2" class="center-align" width="50%"><br><br>
                         ลงชื่อ............................................................<br><br>
                         (............................................................)<br>(ผู้อำนวยการ)<br>วันที่................/................/................</td>
                 </tr>
